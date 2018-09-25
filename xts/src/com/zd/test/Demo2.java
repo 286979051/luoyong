eae6bc7b25cfbd18549252fcdce13506b04f2b8c
Administrator <Administrator@TNJU8U5RRCHIGKG>
2018-09-25 10:22:59 +0800

    首次提交

---
+++ xts/src/com/zd/test/Demo2.java
@@ -0,0 +1,13 @@
+package com.zd.test;
+
+public class Demo2 {
+
+	
+	public static void main(String[] args) {
+		System.out.println(11111);
+		System.out.println("Ğ¡ĞñĞñ");
+		System.out.println("xts");
+		System.out.println("Ğ¤Í¢Ë§");
+		System.out.println("ÀÏÍõ");
+	}
+}
