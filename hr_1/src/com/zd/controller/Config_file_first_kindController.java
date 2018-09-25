29c4868c4e4eb68fe5d760ed9b0a29fd4ac7a6e8
罗勇 <@DESKTOP-89KRDNG>
2018-09-25 17:38:20 +0800

    一级机构联动

---
+++ hr_1/src/com/zd/controller/Config_file_first_kindController.java
@@ -0,0 +1,25 @@
+package com.zd.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zd.entity.Config_file_first_kind;
+import com.zd.service.IConfig_file_first_kindmapperService;
+
+
+@Controller
+public class Config_file_first_kindController {
+	@Autowired
+	private IConfig_file_first_kindmapperService config_file_first_kindmapperService;
+	
+	@RequestMapping("/query")
+	public String query(Map map) {
+		List<Config_file_first_kind> firstList = config_file_first_kindmapperService.query();
+		map.put("firstList", firstList);
+		return "/recruit/position/position_register";
+	}
+}
